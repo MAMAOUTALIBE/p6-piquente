9af482574a8383beaa266a59d907ea8b06703bea
MAMAOUTALIBE <103372164+MAMAOUTALIBE@users.noreply.github.com>
2023-02-12 11:14:08 +0100

    CORRECTION ERREUR

--- app.js
+++ app.js
@@ -1,31 +1,41 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-
-const stuffRoutes = require('./routes/stuff');
+const bodyParser = require('body-parser');
+const saucesRoutes = require('./routes/sauce');
 const userRoutes = require('./routes/user');
-
-
-mongoose.connect('mongodb+srv://bahoumou:BAHmamadou@cluster0.mszdlnx.mongodb.net/?retryWrites=true&w=majority',
-  { useNewUrlParser: true,
-    useUnifiedTopology: true })
-  .then(() => console.log('Connexion à MongoDB réussie !'))
-  .catch(() => console.log('Connexion à MongoDB échouée !'));
+const path = require('path');
+const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require("helmet");
 
 const app = express();
 
+require('dotenv').config();
+const user = process.env.DB_USER;
+const pass = process.env.DB_PASS;
+// mongoose connect
+mongoose.connect(`mongodb+srv://${user}:${pass}@sandbox.kcodm.mongodb.net/db_piquante?retryWrites=true&w=majority`,
+    { useNewUrlParser: true,
+     useUnifiedTopology: true 
+    })
+    .then(() => console.log('Connexion à MongoDB réussie !'))
+    .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+// CORS - partage de ressources entre serveurs
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
-  });
-app.use(bodyParser.json());
+});
 
 
-app.use('/api/stuff', stuffRoutes);
+app.use(bodyParser.json()); //bodyparser
+
+app.use(mongoSanitize()); // mongo-sanitize to prevent operator injection
+app.use(helmet()); // helmet
+
+app.use('/images', express.static(path.join(__dirname, 'images')));
+app.use('/api/sauces', saucesRoutes);
 app.use('/api/auth', userRoutes);
 
-
-
-module.exports = app;
+module.exports = app; // export de l'appli
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const app = require('./app');
 
-const normalizePort = val => {
+const normalizePort = val => { // la fonction normalizeport renvoie un port valide, qu'il soit fourni sous la forme d'un number ou d'un string
   const port = parseInt(val, 10);
 
   if (isNaN(port)) {
@@ -12,10 +12,10 @@ const normalizePort = val => {
   }
   return false;
 };
-const port = normalizePort(process.env.PORT || '3000');
+const port = normalizePort(process.env.PORT || '3000');
 app.set('port', port);
 
-const errorHandler = error => {
+const errorHandler = error => { // la fonction errorHandler  recherche les différentes erreurs et les gère de manière appropriée. Elle est ensuite enregistrée dans le serveur ;
   if (error.syscall !== 'listen') {
     throw error;
   }
@@ -37,7 +37,7 @@ const errorHandler = error => {
 
 const server = http.createServer(app);
 
-server.on('error', errorHandler);
+server.on('error', errorHandler); // un écouteur d'évènements est également enregistré, consignant le port ou le canal nommé sur lequel le serveur s'exécute dans la console.
 server.on('listening', () => {
   const address = server.address();
   const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
